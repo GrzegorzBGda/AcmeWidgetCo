fb83c6c68b7d989675aca23558fc0436d117e1ab
Grzegorz Bielski <grzegorz.bielski@brokeria.pl>
2021-10-23 14:50:19 +0200

    Example, how to add 30% off to basket total()
    Decorator added

---
+++ src/Classes/Decorator/BasketDecorator.php
@@ -0,0 +1,105 @@
+<?php
+
+
+namespace AcmeWidgetCo\Classes\Decorator;
+
+
+use AcmeWidgetCo\Classes\Basket;
+use AcmeWidgetCo\Classes\Interfaces\iBasket;
+use AcmeWidgetCo\Classes\Interfaces\iDeliveryChargeRulesStrategy;
+use AcmeWidgetCo\Classes\Interfaces\iOffersStrategy;
+use AcmeWidgetCo\Classes\Interfaces\iProductCatalogue;
+use Exception;
+
+class BasketDecorator implements iBasket
+{
+    /**
+     * @var Basket
+     */
+    private Basket $basket;
+
+    /**
+     * BasketDecorator constructor.
+     * @param Basket $basket
+     */
+    public function __construct(Basket $basket)
+    {
+        $this->basket = $basket;
+    }
+
+    /**
+     * @param string $productCode
+     */
+    public function add(string $productCode): void
+    {
+        $this->basket->add($productCode);
+    }
+
+    /**
+     * @return float
+     * @throws Exception
+     */
+    public function total(): float
+    {
+        return round($this->basket->total() - ($this->basket->total() * 30 / 100), 2);
+    }
+
+    /**
+     * @return array
+     */
+    public function getProducts(): array
+    {
+        return $this->basket->getProducts();
+    }
+
+    /**
+     * @param iProductCatalogue $productCatalogue
+     * @return Basket
+     */
+    public function setProductCatalogue(iProductCatalogue $productCatalogue): Basket
+    {
+        return $this->basket->setProductCatalogue($productCatalogue);
+    }
+
+    /**
+     * @return iProductCatalogue
+     */
+    public function getProductsCatalogue(): iProductCatalogue
+    {
+        return $this->basket->getProductsCatalogue();
+    }
+
+    /**
+     * @param iDeliveryChargeRulesStrategy $deliveryChargeRulesStrategy
+     * @return iBasket
+     */
+    public function setDeliveryChargeRulesStrategy(iDeliveryChargeRulesStrategy $deliveryChargeRulesStrategy): iBasket
+    {
+        return $this->basket->setDeliveryChargeRulesStrategy($deliveryChargeRulesStrategy);
+    }
+
+    /**
+     * @return iDeliveryChargeRulesStrategy
+     */
+    public function getDeliveryChargeRulesStrategy(): iDeliveryChargeRulesStrategy
+    {
+        return $this->basket->getDeliveryChargeRulesStrategy();
+    }
+
+    /**
+     * @return iOffersStrategy
+     */
+    public function getOffersStrategy(): iOffersStrategy
+    {
+        return $this->basket->getOffersStrategy();
+    }
+
+    /**
+     * @param iOffersStrategy $offersStrategy
+     * @return iBasket
+     */
+    public function setOffersStrategy(iOffersStrategy $offersStrategy): iBasket
+    {
+        return $this->basket->setOffersStrategy($offersStrategy);
+    }
+}
