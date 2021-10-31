d5fbaf32fe432330f3320787f363b1e6bdf3c8ca
Grzegorz Bielski <grzegorz.bielski@brokeria.pl>
2021-10-31 08:38:57 +0100

    Exercise text fixed

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-echo "<h1>PHP excercise</h1>";
+echo "<h1>PHP exercise</h1>";
 echo "<h2>String reverse algorithm</h2>";
 
-echo "<p>Revers fallowing string using your own algorithm: Madam I'm Adam </p>";
+echo "<p>Reverse fallowing string using your own algorithm: Madam I'm Adam </p>";
 echo "<p> Expected outcome is: madA m'I madaM </p>";
